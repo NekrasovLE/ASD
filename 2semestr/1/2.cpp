6b5ef179927856feec3f7d524b88b29c0f5ac097
NekrasovLE <nekrasov.le25@gmail.com>
2025-02-18 15:13:45 +0500

    test from computer

--- 2semestr/1/2.cpp
+++ 2semestr/1/2.cpp
@@ -4,11 +4,12 @@
 void print_arr(float* arr, int n)
 {
 	for(int i = 0; i < n; ++i)
-		printf("f\n", *(arr + i));
+		printf("f\n", arr[i]);
 }
 
 
 int main()
 {
 	float *p;
+	printf("Hello World!!!");
 }
