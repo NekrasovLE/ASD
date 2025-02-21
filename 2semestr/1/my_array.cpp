2f591c31568e860d07c0b5f02dc474365dce611e
NekrasovLE <nekrasov.le25@gmail.com>
2025-02-21 13:00:41 +0500

    work on sort

--- 2semestr/1/my_array.cpp
+++ 2semestr/1/my_array.cpp
@@ -96,7 +96,8 @@ void sort_arr(int* arr, int n)
 	int left = 0, right = n - 1, flag = 1, t;
 	while((left < right) && flag == 1)
 	{
-		for(int i = 0; i < right; ++i)
+		flag = 0;
+		for(int i = left; i < right; i++)
 		{
 			if(arr[i] > arr[i + 1])
 			{
@@ -107,7 +108,7 @@ void sort_arr(int* arr, int n)
 			}
 		}
 		right--;
-		for(int i = right; i > left; --i)
+		for(int i = right; i > left; i--)
 		{
 			if(arr[i - 1] > arr[i])
 			{
